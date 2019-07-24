f66fa962d8099a3f21ef302f81a91621cc8b3c8e
vijay <vijay10022@gmail.com>
2019-07-24 16:27:38 +0530

    second time commited

---
+++ Day-3/loops.c
@@ -0,0 +1,13 @@
+	  1.ADD
+	  2.SUB
+	  3.MUL
+	  4.DIV
+	  5.Mod
+input:3
+	10 3
+output:Multiplication of 10 and 3 is 30
+input:4
+	  10 3
+output:Division of 10 and 4 is 2
+input:6
+output:grater than 5
